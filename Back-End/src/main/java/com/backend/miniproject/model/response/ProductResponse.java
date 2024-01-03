eca850edc4aec62a6955d723de62542e4ed15df2
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 21:57:17 +0700

    Create ProductResponse.java

---
+++ Back-End/src/main/java/com/backend/miniproject/model/response/ProductResponse.java
@@ -0,0 +1,18 @@
+package com.backend.miniproject.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private Long price;
+}
