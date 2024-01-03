e4ab3ba63ad84a8a8da9f4772d21a3fdb57a0f2f
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 09:42:41 +0700

    init project spring boot

---
+++ Back-End/src/main/java/com/backend/miniproject/MiniprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectBackendApplication.class, args);
+	}
+
+}
