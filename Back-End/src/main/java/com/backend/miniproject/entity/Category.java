83cc15bbb3726a3ff799560da79d93d9a6bd3728
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 19:56:26 +0700

    Create Category

--- Back-End/src/main/java/com/backend/miniproject/entity/Category.java
+++ Back-End/src/main/java/com/backend/miniproject/entity/Category.java
@@ -1,6 +1,5 @@
 package com.backend.miniproject.entity;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,24 +11,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "products")
-public class Product {
+@Table(name = "category")
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
     private String name;
-
-    @Column(name = "description")
-    private String description;
-
-    @Column(name = "category")
-    private String category;
-
-    @Column(name = "price")
-    private int price;
-
-    @Column(name = "stock")
-    private int stock;
 }
