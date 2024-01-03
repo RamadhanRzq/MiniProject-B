c8d142a1611f11c89af9b88c6052f08664be00b4
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 10:02:14 +0700

    Update Product.java

--- Back-End/src/main/java/com/backend/miniproject/entity/Product.java
+++ Back-End/src/main/java/com/backend/miniproject/entity/Product.java
@@ -1,6 +1,7 @@
 package com.backend.miniproject.entity;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,11 +11,26 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "Products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
+    @Column(name = "Name")
     private String Name;
+
+    @Column(name = "Description")
     private String Description;
+
+    @Column(name = "Category")
     private String Category;
+
+    @Column(name = "Price")
     private int Price;
+
+    @Column(name = "Stock")
     private int Stock;
 }
