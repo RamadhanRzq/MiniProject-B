b83d76a70ef53eb167f04593c20eb15615a0c17e
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 11:05:49 +0700

    Update ProductController.java

--- Back-End/src/main/java/com/backend/miniproject/controller/ProductController.java
+++ Back-End/src/main/java/com/backend/miniproject/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.backend.miniproject.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -21,5 +18,11 @@ public class ProductController {
     public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto){
         ProductDto savedProduct = productService.createProduct(productDto);
         return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{Id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable("id") Long productId){
+        ProductDto productDto = productService.getProductById(productId);
+        return ResponseEntity.ok(productDto);
     }
 }
