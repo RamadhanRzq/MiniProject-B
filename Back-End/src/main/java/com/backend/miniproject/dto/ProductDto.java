a10252ee71567ad87a2b1f8f91cf3f78e6880c21
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 21:15:09 +0700

    Update ProductDto.java

--- Back-End/src/main/java/com/backend/miniproject/dto/ProductDto.java
+++ Back-End/src/main/java/com/backend/miniproject/dto/ProductDto.java
@@ -10,10 +10,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDto {
-    private Long Id;
-    private String Name;
-    private String Description;
-    private String Category;
-    private int Price;
-    private int Stock;
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private int price;
+    private int stock;
 }
