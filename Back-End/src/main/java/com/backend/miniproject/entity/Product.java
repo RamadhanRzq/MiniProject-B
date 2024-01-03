c6970dd381782ec149eaf548c3c9c79a95d48f61
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 09:58:35 +0700

    Update Product.java

--- Back-End/src/main/java/com/backend/miniproject/entity/Product.java
+++ Back-End/src/main/java/com/backend/miniproject/entity/Product.java
@@ -1,5 +1,15 @@
 package com.backend.miniproject.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     private Long Id;
     private String Name;
