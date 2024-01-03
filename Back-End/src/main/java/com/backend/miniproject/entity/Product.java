fd24c3a65259cda903409c63bb531704f5217d7e
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 09:55:06 +0700

    Update Product.java

--- Back-End/src/main/java/com/backend/miniproject/entity/Product.java
+++ Back-End/src/main/java/com/backend/miniproject/entity/Product.java
@@ -1,4 +1,10 @@
 package com.backend.miniproject.entity;
 
 public class Product {
+    private Long Id;
+    private String Name;
+    private String Description;
+    private String Category;
+    private int Price;
+    private int Stock;
 }
