076a9638855e0eb57ba2f8d3d804c9b7fcf4edab
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 21:54:33 +0700

    Create ProductRequest.java

---
+++ Back-End/src/main/java/com/backend/miniproject/model/request/ProductRequest.java
@@ -0,0 +1,15 @@
+package com.backend.miniproject.model.request;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private String name;
+    private String description;
+    private String category;
+    private int price;
+    private int stock;
+}
