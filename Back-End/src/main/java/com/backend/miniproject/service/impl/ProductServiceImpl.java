6eabf8b3b07f4c00b6170cccfc3d0d8c03281d3b
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 10:22:48 +0700

    Update ProductServiceImpl.java

--- Back-End/src/main/java/com/backend/miniproject/service/impl/ProductServiceImpl.java
+++ Back-End/src/main/java/com/backend/miniproject/service/impl/ProductServiceImpl.java
@@ -1,11 +1,20 @@
 package com.backend.miniproject.service.impl;
 
 import com.backend.miniproject.dto.ProductDto;
+import com.backend.miniproject.entity.Product;
+import com.backend.miniproject.mapper.ProductMapper;
+import com.backend.miniproject.repository.ProductRepository;
 import com.backend.miniproject.service.ProductService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
     @Override
     public ProductDto createProduct(ProductDto productDto){
-        return null;
+        Product product = ProductMapper.mapToProduct(productDto);
+        Product savedProduct = productRepository.save(product);
+        return ProductMapper.mapToProductDto(savedProduct);
     }
 }
