3da078bb9024e91c6a579aca1e123819b1e6b916
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-03 10:05:05 +0700

    Create ProductRepository.java

---
+++ Back-End/src/main/java/com/backend/miniproject/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.backend.miniproject.repository;
+
+import com.backend.miniproject.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
