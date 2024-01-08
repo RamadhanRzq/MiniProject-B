d0d31482abf5624a0e0157eddfd7f8e98c883d9b
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-08 13:39:14 +0700

    Upload File Gambar, Harusnya Berhasil 😞

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -9,7 +9,6 @@ import CategoryList from "./pages/CategoryList";
 import ProductForm from "./pages/ProductForm";
 import ProductList from "./pages/ProductList";
 import Update from "./pages/Update";
-import TestUpload from "./pages/TestUpload";
 
 function App() {
   return (
@@ -25,7 +24,6 @@ function App() {
           <Route path="/update/:id" element={<Update />} />
           <Route path="/add" element={<CategoryForm />} />
           <Route path="/categories" element={<CategoryList />} />
-          <Route path="/test" element={<TestUpload />} />
         </Routes>
       </Layout>
     </div>
