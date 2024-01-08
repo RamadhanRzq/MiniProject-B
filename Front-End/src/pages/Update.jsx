966aa711335a3a7ecd4ddd02604e0c7845a902c4
Rifdah Kamilah <rifkamilah@gmail.com>
2024-01-08 19:47:43 +0700

    updated

--- Front-End/src/pages/Update.jsx
+++ Front-End/src/pages/Update.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+import { MdAddPhotoAlternate } from "react-icons/md";
 import { yupResolver } from "@hookform/resolvers/yup";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -98,104 +98,190 @@ function Update() {
 
   return (
     <section className="px-20 ml-96">
-      <h1 className="text-3xl font-semibold">Update Product Form</h1>
-      <div className="grid grid-cols-2 gap-20 mt-8">
+      <div className="grid grid-cols-2 gap-20 mt-4">
         <div className="w-[500px]">
-          <h2>New Product</h2>
-          <hr />
+          <h2 className="font-bold text-2xl">Edit Produk</h2>
           <form
             className="flex flex-col gap-4 mt-4"
             onSubmit={handleSubmit(onSubmitForm)}
+            encType="multipart/form-data"
           >
-            <div>
-              <label htmlFor="name">Product Name</label>
-              <input
-                placeholder="Product Name"
-                className="w-full rounded-lg border-[1px] border-gray-200 p-4 pe-12 text-sm focus:outline-sky-200"
-                {...register("name")}
-                id="name"
-              />
-              <p className="error text-red-600">{errors.name?.message}</p>
+            {/* Product Name */}
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Nama Produk
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="name"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Nama Produk"
+                    {...register("name")}
+                  />
+                </div>
+                <p className="error text-red-600">{errors.name?.message}</p>
+              </div>
             </div>
 
-            <div>
-              <label htmlFor="image">Product Image</label>
-              <input
-                placeholder="Product Image"
-                className="w-full rounded-lg border-[1px] border-gray-200 p-4 pe-12 text-sm focus:outline-sky-200"
-                {...register("image")}
-                id="image"
-              />
+            {/* Product Image */}
+            <div className="col-span-full">
+              <label
+                htmlFor="image"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Gambar Produk
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <MdAddPhotoAlternate
+                    className="mx-auto h-12 w-12 text-gray-300"
+                    aria-hidden="true"
+                  />
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>Upload a file</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                        {...register("image")}
+                      />
+                    </label>
+                    <p className="pl-1">or drag and drop</p>
+                  </div>
+                  <p className="text-xs leading-5 text-gray-600">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
+                </div>
+              </div>
               <p className="error text-red-600">{errors.image?.message}</p>
             </div>
 
-            <div>
-              <label htmlFor="price">Product Price</label>
-              <input
-                placeholder="Product Price"
-                className="w-full rounded-lg border-[1px] border-gray-200 p-4 pe-12 text-sm focus:outline-sky-200"
-                {...register("price")}
-                id="price"
-              />
-              <p className="error text-red-600">{errors.price?.message}</p>
-            </div>
-
-            <div>
-              <label htmlFor="stock">Product Stock</label>
-              <input
-                placeholder="Product Stock"
-                className="w-full rounded-lg border-[1px] border-gray-200 p-4 pe-12 text-sm focus:outline-sky-200"
-                {...register("stock")}
-                id="stock"
-              />
-              <p className="error text-red-600">{errors.stock?.message}</p>
-            </div>
-
-            <div>
-              <label htmlFor="categoryId">Select Category:</label>
-              <select
-                id="categoryId"
-                value={selectedCategory}
-                className="w-full rounded-lg border-[1px] border-gray-200 p-4 pe-12 text-sm focus:outline-sky-200"
-                {...register("categoryId")}
-                onChange={handleCategoryChange}
+            {/* Product Price */}
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="price"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
-                <option value="">Select Categories Here</option>
-                {categories.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                ))}
-              </select>
-              {selectedCategory && (
-                <p>
-                  Selected Category:{" "}
-                  {
-                    categories.find(
-                      (category) =>
-                        category.id === parseInt(selectedCategory, 10)
-                    ).name
-                  }
-                </p>
-              )}
+                Harga Produk
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    autoComplete="price"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Harga Produk"
+                    {...register("price")}
+                  />
+                </div>
+                <p className="error text-red-600">{errors.price?.message}</p>
+              </div>
             </div>
 
-            <button
-              className="border border-white rounded-lg bg-sky-400 p-2 text-white self-center w-full"
-              type="submit"
-            >
-              Update Product
-            </button>
+            {/* Product Stock */}
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="stock"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Stok Produk
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="stock"
+                    id="stock"
+                    autoComplete="stock"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Stok Produk"
+                    {...register("stock")}
+                  />
+                </div>
+                <p className="error text-red-600">{errors.stock?.message}</p>
+              </div>
+            </div>
+
+            {/* Product Category */}
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="categoryId"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Pilih Kategori
+              </label>
+              <div className="mt-2">
+                <select
+                  id="categoryId"
+                  value={selectedCategory}
+                  name="categoryId"
+                  autoComplete="categoryId"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  {...register("categoryId")}
+                  onChange={handleCategoryChange}
+                >
+                  <option value="">Pilih Kategori</option>
+                  {categories.map((category) => (
+                    <option key={category.id} value={category.id}>
+                      {category.name}
+                    </option>
+                  ))}
+                </select>
+                {selectedCategory && (
+                  <p>
+                    Selected Category:{" "}
+                    {
+                      categories.find(
+                        (category) =>
+                          category.id === parseInt(selectedCategory, 10)
+                      ).name
+                    }
+                  </p>
+                )}
+              </div>
+            </div>
+
+            {/* Save and Cancel Buttons */}
+            <div className="mt-6 flex items-center justify-start gap-x-6">
+              <button
+                type="button"
+                className="rounded-md bg-white text-indigo-600 px-3 py-2 text-sm font-semibold shadow-sm hover:text-white hover:bg-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Batal
+              </button>
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Simpan
+              </button>
+            </div>
+
+            {/* Back to Product List Link */}
+            <div className="mt-4">
+              <Link
+                to="/categories"
+                className="text-sky-600 hover:underline cursor-pointer"
+              >
+                Kembali ke Daftar Produk
+              </Link>
+            </div>
           </form>
         </div>
-      </div>
-      <div className="mt-4">
-        <Link
-          to="/categories"
-          className="text-sky-600 hover:underline cursor-pointer"
-        >
-          Back to Product List
-        </Link>
       </div>
     </section>
   );
