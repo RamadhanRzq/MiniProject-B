403808bb03d0dd8fdf8286d697a2851eab4894a6
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-09 10:50:04 +0700

    Nambah Pop Up Checkout

--- Front-End/src/pages/PopUp.jsx
+++ Front-End/src/pages/PopUp.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function PopUp({ onCancel }) {
   return (
-    <div className="flex min-h-screen justify-center items-center promptdelete ">
+    <div className="flex min-h-screen justify-center items-center popupstyle ">
       <div className="prompt-content bg-white p-10 rounded-lg ">
         <p className="text-black">Thank You !</p>
         <div className="flex prompt-buttons gap-10 justify-center mt-10">
